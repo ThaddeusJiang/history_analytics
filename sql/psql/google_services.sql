74be507715067d2f2194c2e842110a85c7f90754
TJ <thaddeusjiang@gmail.com>
2024-10-01 10:33:09 +0900

    fix: sql like matched query-string, I'd like to only match domain

--- sql/psql/google_services.sql
+++ sql/psql/google_services.sql
@@ -1,7 +1,14 @@
-SELECT
-    SUBSTRING(u.url FROM 'https?://([^/]+)') AS domain,
-    SUM(u.visit_count) AS total_visits
-FROM urls u
-WHERE url LIKE '%.google.com%'
-GROUP BY domain
-ORDER BY total_visits DESC
+WITH _google_domains AS (
+	SELECT
+		SUBSTRING(url FROM 'https?://([^/]+)') as DOMAIN,
+		SUM(visit_count) AS total_visit_count
+	FROM urls
+	WHERE url LIKE '%.google.com%'
+	GROUP BY DOMAIN
+	ORDER BY total_visit_count DESC
+)
+
+SELECT DOMAIN, total_visit_count
+FROM _google_domains
+WHERE DOMAIN LIKE '%google.com%'
+order by total_visit_count
